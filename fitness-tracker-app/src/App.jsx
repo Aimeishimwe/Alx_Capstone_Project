c63b6921f6dc07b03e3867a96f5ceefd87622025
Aime Ishimwe <medicalmind07@gmail.com>
2025-01-09 02:04:58 +0200

    Finalized the routing system and paths for all screens

--- fitness-tracker-app/src/App.jsx
+++ fitness-tracker-app/src/App.jsx
@@ -1,46 +1,67 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import WelcomePage from "./components/screens/WelcomePage";
-import Onboarding1 from "./components/screens/Onboarding1";
-import Onboarding2 from "./components/screens/Onboarding2";
-import Onboarding3 from "./components/screens/Onboarding3";
-import LogIn from "./components/screens/LogIn";
-import SignUp from "./components/screens/SignUp";
-import ForgotPassword from "./components/screens/ForgotPassword";
-import SetupPassword from "./components/screens/SetupPassword";
-import HomePage from "./components/screens/HomePage";
-import LogWorkout from "./components/screens/LogWorkout";
-import WorkoutHistory from "./components/screens/WorkoutHistory";
-import UserProfile from "./components/screens/UserProfile";
-import NutritionTracking from "./components/screens/NutritionTracking";
-import ProgressTracking from "./components/screens/ProgressTracking";
-import WorkoutPlan from "./components/screens/WorkoutPlan";
-import CreateWorkoutPlan from "./components/screens/CreateWorkoutPlan";
-import LogNutrition from "./components/screens/LogNutrition";
+
+// Import screens
+import WelcomePage from "./components/screens/Onboarding/WelcomePage";
+import Onboarding1 from "./components/screens/Onboarding/Onboarding1";
+import Onboarding2 from "./components/screens/Onboarding/Onboarding2";
+import Onboarding3 from "./components/screens/Onboarding/Onboarding3";
+import LogInPage from "./components/screens/Auth/LogInPage";
+import SignUpPage from "./components/screens/Auth/SignUpPage";
+import ForgotPasswordPage from "./components/screens/Auth/ForgotPasswordPage";
+import SetupPasswordPage from "./components/screens/Auth/SetupPasswordPage";
+import HomePage from "./components/screens/Main/HomePage";
+import LogWorkoutPage from "./components/screens/Main/LogWorkoutPage";
+import WorkoutHistoryPage from "./components/screens/Main/WorkoutHistoryPage";
+import UserProfilePage from "./components/screens/Main/UserProfilePage";
+import NutritionTrackingPage from "./components/screens/Main/NutritionTrackingPage";
+import ProgressTrackingPage from "./components/screens/Main/ProgressTrackingPage";
+import CreateWorkoutPlanPage from "./components/screens/Main/CreateWorkoutPlanPage";
+import LogNutritionPage from "./components/screens/Main/LogNutritionPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Onboarding routes */}
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/Onboarding1" element={<Onboarding1 />} />
-        <Route path="/Onboarding2" element={<Onboarding2 />} />
-        <Route path="/Onboarding3" element={<Onboarding3 />} />
-        <Route path="/LogIn" element={<LogIn />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/ForgotPassword" element={<ForgotPassword />} />
-        <Route path="/SetupPassword" element={<SetupPassword />} />
-        <Route path="/HomePage" element={<HomePage />} />
-        <Route path="/LogWorkout" element={<LogWorkout />} />
-        <Route path="/WorkoutHistory" element={<WorkoutHistory />} />
-        <Route path="/UserProfile" element={<UserProfile />} />
-        <Route path="/NutritionTracking" element={<NutritionTracking />} />
-        <Route path="/ProgressTracking" element={<ProgressTracking />} />
-        <Route path="/WorkoutPlan" element={<WorkoutPlan />} />
-        <Route path="/CreateWorkoutPlan" element={<CreateWorkoutPlan />} />
-        <Route path="/LogNutrition" element={<LogNutrition />} />
+        <Route path="/onboarding1" element={<Onboarding1 />} />
+        <Route path="/onboarding2" element={<Onboarding2 />} />
+        <Route path="/onboarding3" element={<Onboarding3 />} />
+
+        {/* Authentication routes */}
+        <Route path="/Auth/LogInPage" element={<LogInPage />} />
+        <Route path="/Auth/SignUpPage" element={<SignUpPage />} />
+        <Route
+          path="/Auth/ForgotPasswordPage"
+          element={<ForgotPasswordPage />}
+        />
+        <Route path="/Auth/setupPassword" element={<SetupPasswordPage />} />
+
+        {/* Main app routes */}
+        <Route path="/Main/HomePage" element={<HomePage />} />
+        <Route path="/Main/LogWorkoutPage" element={<LogWorkoutPage />} />
+        <Route
+          path="/Main/WorkoutHistoryPage"
+          element={<WorkoutHistoryPage />}
+        />
+        <Route path="/Main/UserProfilePage" element={<UserProfilePage />} />
+        <Route
+          path="/Main/NutritionTrackingPage"
+          element={<NutritionTrackingPage />}
+        />
+        <Route
+          path="/Main/ProgressTrackingPage"
+          element={<ProgressTrackingPage />}
+        />
+        <Route
+          path="/Main/CreateWorkoutPlanPage"
+          element={<CreateWorkoutPlanPage />}
+        />
+        <Route path="/Main/LogNutritionPage" element={<LogNutritionPage />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
