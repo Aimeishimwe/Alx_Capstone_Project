bf4bdc5094a00e6dff730cc502282f192021e019
Aime Ishimwe <medicalmind07@gmail.com>
2025-01-10 02:41:55 +0200

    made the button responsive

--- fitness-tracker-app/src/components/common/Button.jsx
+++ fitness-tracker-app/src/components/common/Button.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 function Button({ label, to }) {
   return (
     <Link to={to}>
-      <button className="mt-8 px-6 py-3 bg-blue-500 text-black font-bold rounded-full hover:bg-blue-700 transition duration-300 w-60">
+      <button className="mt-8 px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 bg-blue-500 text-sm sm:text-base md:text-lg lg:text-xl text-black font-bold rounded-full hover:bg-blue-700 transition duration-300 w-40 sm:w-48 md:w-60 lg:w-72">
         {label}
       </button>
     </Link>
   );
 }
+
 export default Button;
